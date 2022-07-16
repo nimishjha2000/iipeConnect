2f9416d618d81f376d218b9d95c82be20c010783
nimishjha2000 <69070093+nimishjha2000@users.noreply.github.com>
2022-07-16 16:50:30 +0530

    Add files via upload

---
+++ src/pages/r/[communityId]/submit.tsx
@@ -0,0 +1,40 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useRecoilValue } from 'recoil';
+import { communityState } from '../../../atoms/communitiesAtom';
+import About from '../../../components/Community/About';
+import PageContent from '../../../components/Layout/PageContent';
+import NewPostForm from '../../../components/Posts/NewPostForm';
+import { auth } from '../../../firebase/clientApp';
+import useCommunityData from '../../../hooks/useCommunityData';
+
+
+
+const SubmitPostPage:React.FC = () => {
+    const [user] = useAuthState(auth);
+    //const communityStateValue = useRecoilValue(communityState);
+    const { communityStateValue } = useCommunityData();
+    console.log("Community", communityStateValue);
+    
+    
+    return (
+        <PageContent>
+            {/* <> NewPostForm </> */}
+            <>
+                <Box p="14px 0px" borderBottom="1px solid" borderColor="white" >
+                    <Text fontWeight={600} > Create a post </Text>
+                    
+                </Box>
+                {user && <NewPostForm user={user} communityImageURL={communityStateValue.currentCommunity?.imageURL} />}
+            
+            </>
+            {/* <> About </> */}
+            { communityStateValue.currentCommunity && <About communityData={communityStateValue.currentCommunity} />}
+            <>
+            
+            </>
+        </PageContent>
+    )
+}
+export default SubmitPostPage
