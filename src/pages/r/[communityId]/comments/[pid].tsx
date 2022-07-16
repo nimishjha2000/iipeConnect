1a88b9733511c088965f368d5de24fd5ef399122
nimishjha2000 <69070093+nimishjha2000@users.noreply.github.com>
2022-07-16 16:49:35 +0530

    Add files via upload

---
+++ src/pages/r/[communityId]/comments/[pid].tsx
@@ -0,0 +1,71 @@
+import { User } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Post } from "../../../../atoms/postsAtom";
+import About from "../../../../components/Community/About";
+import PageContent from '../../../../components/Layout/PageContent';
+import Comments from "../../../../components/Posts/Comments/Comments";
+import PostItem from '../../../../components/Posts/PostItem';
+import { auth, firestore } from '../../../../firebase/clientApp';
+import useCommunityData from "../../../../hooks/useCommunityData";
+import usePosts from '../../../../hooks/usePosts';
+
+
+
+const PostPage:React.FC= () => {
+    const [user] = useAuthState(auth);
+    const { postStateValue, setPostStateValue, onDeletePost, onVote } = usePosts();
+    const router = useRouter();
+    const { communityStateValue } = useCommunityData();
+
+    const fetchPost = async (postId: string) => {
+        console.log("FETCHING POST");
+    
+        //setLoading(true);
+        try {
+          const postDocRef = doc(firestore, "posts", postId);
+          const postDoc = await getDoc(postDocRef);
+          setPostStateValue((prev) => ({
+            ...prev,
+            selectedPost: { id: postDoc.id, ...postDoc.data() } as Post,
+          }));
+          
+        } catch (error: any) {
+          console.log("fetchPost error", error.message);
+        }
+        //setLoading(false);
+      };
+
+
+    useEffect(() => {
+        const { pid } = router.query;
+    
+        if (pid && !postStateValue.selectedPost) {
+          fetchPost(pid as string);
+        }
+      }, [router.query, postStateValue.selectedPost]);
+
+    return (
+        <PageContent>
+            <>
+                {/* SelectedPost */}
+            { postStateValue.selectedPost &&  <PostItem  post={postStateValue.selectedPost} onVote={onVote} onDeletePost={onDeletePost} userVoteValue={postStateValue.postVotes.find((item) => item.postId === postStateValue.selectedPost?.id)?.voteValue}  userIsCreator={user?.uid === postStateValue.selectedPost?.creatorId}  />}
+                {/* Comments */}
+                <Comments user={user as User} selectedPost={postStateValue.selectedPost} communityId={postStateValue.selectedPost?.communityId as string} />
+          
+          
+            
+            
+            </>
+            <> 
+                {/* About */}
+            { communityStateValue.currentCommunity && <About communityData={communityStateValue.currentCommunity} />}
+            </>
+
+            
+        </PageContent>
+    )
+}
+export default PostPage;
