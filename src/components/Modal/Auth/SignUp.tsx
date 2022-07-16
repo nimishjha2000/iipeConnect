e899bb4904553c6b0c04170d42ef9c9f1d14a19b
nimishjha2000 <69070093+nimishjha2000@users.noreply.github.com>
2022-07-16 16:36:40 +0530

    Add files via upload

---
+++ src/components/Modal/Auth/SignUp.tsx
@@ -0,0 +1,74 @@
+import { Input, Button, Flex, Text} from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { authModalState } from '../../../atoms/authModalAtom';
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth, firestore } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
+import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import { User } from 'firebase/auth';
+
+const SignUp:React.FC = () => {
+    
+    const setAuthModalState = useSetRecoilState(authModalState);
+    const [singUpForm, setSignUpForm] = useState({
+        email: "",
+        password: "",
+        confirmPassword: "",
+    });
+    const [error, setError] = useState('');
+    const [
+        createUserWithEmailAndPassword,
+        userCred,
+        loading,
+        userError,
+      ] = useCreateUserWithEmailAndPassword(auth);
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (error) setError(" ");
+        if (singUpForm.confirmPassword!== singUpForm.password){
+            setError("Passwords do not match");
+            return;
+        }
+        createUserWithEmailAndPassword(singUpForm.email, singUpForm.confirmPassword);
+    };
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        // update form state
+        setSignUpForm(prev => ({
+            ...prev,
+            [event.target.name]: event.target.value,
+        }))
+    };
+    const createUserDocument = async (user: User) => {
+        const userDocRef = doc(firestore, "users", user.uid);
+        await setDoc(userDocRef, JSON.parse(JSON.stringify(user)));
+
+    };
+
+    useEffect(() => {
+        if (userCred){
+            createUserDocument(userCred.user);
+        }
+    }, [userCred]);
+
+
+    return(
+        <form onSubmit={onSubmit}>
+            <Input required name='email' placeholder='email' type='email' mb={2} onChange={ onChange} fontSize='10pt' _placeholder={{color: 'gray.500'}} _hover={{bg: 'white', border: ' 1px solid', borderColor: 'blue.500'}} _focus={{outline: "none", bg: 'white', border: ' 1px solid', borderColor: 'blue.500' }} bg="gray.50"  />
+            <Input required name='password' placeholder='password' type='password' mb={2} onChange={ onChange} fontSize='10pt' _placeholder={{color: 'gray.500'}} _hover={{bg: 'white', border: ' 1px solid', borderColor: 'blue.500'}} _focus={{outline: "none", bg: 'white', border: ' 1px solid', borderColor: 'blue.500' }} bg="gray.50"  />
+            <Input required name='confirmPassword' placeholder='password' type='password' mb={2} onChange={ onChange} fontSize='10pt' _placeholder={{color: 'gray.500'}} _hover={{bg: 'white', border: ' 1px solid', borderColor: 'blue.500'}} _focus={{outline: "none", bg: 'white', border: ' 1px solid', borderColor: 'blue.500' }} bg="gray.50"  />
+            <Text textAlign='center' color="" fontSize="10pt ">Password should be at least 6 characters</Text>
+            <Text textAlign="center" mt={2} fontSize="10pt" color="red">{error || FIREBASE_ERRORS[userError?.message as keyof typeof FIREBASE_ERRORS]}</Text>
+            <Button width='100%' height='36px' mt={2} mb={2} type='submit' isLoading={loading}>Sign Up </Button>
+            <Flex fontSize="9pt" justifyContent="center">
+                <Text mr={1}>Already a IIPEConnectian</Text>
+                <Text color="blue.500" fontWeight={700} cursor="pointer" onClick={() => setAuthModalState((prev) => ({
+                    ...prev,
+                    view: 'login',
+                }))}>LOG IN</Text>
+            </Flex>
+        </form>
+    );
+}
+export default SignUp;
